b17567888972e73fb9258496140d934a50670276
haditariq <hadi-tariq@live.co.uk>
2020-12-21 16:09:40 +0500

    submission: Interview Hadi Tariq

--- inactive.js
+++ inactive.js
@@ -17,8 +17,22 @@ const inactiveThirdStage = () => {
     console.log("User has been inactive for the past 5-6 minutes");
 }
 
+const milliInOne = 60000;
 
 // this is the main method that acts like a worker and runs every 1 minute
 setInterval(function(){
     // please write your code here
-}, 1000);
+    const difference = Date.now() - user.lastActivity;
+
+    if(difference >= milliInOne && difference <= milliInOne * 2){
+        inactiveFirstStage();
+    }else if(difference >= milliInOne * 2 && difference <= milliInOne * 3){
+        inactiveSecondStage();
+    }else if(difference >= milliInOne * 4 && difference <= milliInOne * 5){
+        inactiveThirdStage();
+    }else{
+        user.lastActivity = new Date().getTime();
+        inactiveFirstStage();
+    }
+
+}, milliInOne);
